3c77ccde114e0db34c38e59fd7d67a7d3322d08d
Mobil Game 06 <nico.schnass@gmail.com>
2022-10-06 16:23:19 +0200

    Update server.lua

--- dns/server.lua
+++ dns/server.lua
@@ -29,7 +29,7 @@ for address, _ in component.list("modem", false) do
   local t1 = thread.create(function()    
     readcmd = true
     while readcmd do
-    term.setCursor(1,49)
+    term.setCursor(1,50)
     input = io.read()
     if input == "reboot" then
       os.execute("reboot")
@@ -38,19 +38,16 @@ for address, _ in component.list("modem", false) do
     elseif input == "reload" then
       settings = ttf.load(path)
       term.write(cprefix.. "Reloaded\n")
-      term.write("\n")
     elseif input == "help" then
       term.write("------------Help-------------\n")
       term.write(cprefix.. "[reboot] reboot your pc\n")
       term.write(cprefix.. "[stop] shutdown your pc\n")
       term.write(cprefix.. "[reload] reload the config\n")
       term.write(cprefix.. "[create] create a dns entry\n")
-      term.write("\n")
     elseif input == "create" then
       create()
     else 
       term.write(cprefix.. "This command don't exist try to use help\n")
-      term.write("\n")
     end 
   end
   end)
@@ -71,11 +68,12 @@ function create()
 end
 
 
+
   term.write(sprefix.. "Server started\n")
   term.write(sprefix.. "Listening on port: " ..port.."\n")
   term.write(sprefix.. "Adress: ")
   term.write(localAddress .."\n")
-  term.write("\n")
+  
 
   
   while true do
@@ -84,12 +82,10 @@ end
       local param = string.gsub(tostring(param), '\n', '')
       term.clearLine()
       term.write(sprefix.. "Request from "..from .."\n")
-      term.write("\n")
       if command == "lookup" then
         addr = tostring(settings[param])
         term.clearLine()
         term.write(sprefix.. 'DNS Lookup: '.. param .. ' -> ' .. addr)
-        term.write("\n")
         term.write("\n")
         modem.send(from, port, addr)
       end     
